0fda858a3aff356dee84dd764718f551259daad5
Ethan Cai <ccy-3253@163.com>
2014-11-19 17:33:04 +0800

    remove NetIS related code

--- Service.Content/server/const.js
+++ Service.Content/server/const.js
@@ -2,16 +2,25 @@
  * Created by Dev on 2014/9/25.
  */
 
+// Const
+var CUSTOMIZED_META_PREFIX = "x-fs-meta-";
+var PARAM_BUCKET = 'bucket';
+var PARAM_OVERWRITE = "overwrite";
+var PARAM_OVERWRITE_META = "overwrite-meta";
+var PARAM_WRITE_META_ONLY = "write-meta-only";
+var PARAM_KEY = "key";
+var PARAM_FILE = 'file';
+var PARAM_CONTENT_MD5 = "Content-MD5";
+
 module.exports = {
 
-    "NetISErrorCode" : {
-        "VALIDATION_ERROR" : "validation_error",
-        "LOGIC_ERROR" : "logic_error",
-        "OPERATIONAL_ERROR" : "operational_error"
-    },
+    "CUSTOMIZED_META_PREFIX" : "x-fs-meta-",
+    "PARAM_BUCKET" : "bucket",
+    "" : "",
+    "" : "",
+    "" : "",
+    "" : "",
 
-    "LogicErrorAbbr" : {
-        "SESSION_NOT_EXIST" : "session_not_exist",
-        "CACHE_NOT_EXIST" : "cache_not_exist"
-    }
 };
+
+
